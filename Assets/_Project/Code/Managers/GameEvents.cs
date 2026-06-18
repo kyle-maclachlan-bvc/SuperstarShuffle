c23f4f37739fadd85af68930b343290b0da8d424
Kyle MacLachlan <dyrezzstudios@gmail.com>
2026-06-18 12:51:20 -0600

    Refactor: Expanded GameEvents Infrastructure

--- Assets/_Project/Code/Managers/GameEvents.cs
+++ Assets/_Project/Code/Managers/GameEvents.cs
@@ -2,12 +2,30 @@ using System;
 
 public static class GameEvents
 {
+    
+    // Game Flow
     public static Action<GameState> OnGameStateChange;
+    public static Action OnBoardReady;
+    
+    // Turn flow
+    public static Action<Player> OnTurnStarted;
+    public static Action<Player> OnTurnEnded;
+    
+    // Movement
+    public static Action<Player, int> OnDiceRolled;
+    public static Action<Player> OnMovementStarted;
+    public static Action<Player> OnMovementFinished;
+    
+    // board
+    public static Action<Player, BoardSpace> OnSpaceLanded;
+    
+    // Minigames
+    public static Action<PlayerData> OnMinigameWinnerDeclared;
+    public static Action OnMinigameStarted;
+    public static Action OnMinigameEnded;
+
     // Invokes whenever the game's current state changes.
     // Subscribers may use this event to update UI, enable systems, or react to game flow changes.
-
-    //public static Action<int> OnTurnChange;
-    // Planned event for notifying systems when the active player's turn changes.
 
     //public static Action<Player> OnStarPurchased;
     // Planned event for notifying systems when a player purchases a star
@@ -17,9 +35,9 @@ public static class GameEvents
 
     //public static Action OnMinigameStarted;
     // planned event for notifying systems when a minigame begins
-    
+
     //public static Action OnMinigameEnded;
     // planned event for notifying systems when a minigame ends.
-    
-    public static Action OnBoardReady;
+
+
 }
